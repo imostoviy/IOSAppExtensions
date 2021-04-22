56e2a38313cf5023d34b67af308e680f5838263c
imostoviy <ihor.mostovyi@ekreative.com>
2021-04-23 01:35:24 +0300

    add app delegate and remote notifications

--- IOSAppExtensions/IOSAppExtensionsApp.swift
+++ IOSAppExtensions/IOSAppExtensionsApp.swift
@@ -6,12 +6,53 @@
 //
 
 import SwiftUI
+import os
 
 @main
 struct IOSAppExtensionsApp: App {
+    @UIApplicationDelegateAdaptor private var appDelegate: AppDelegate
     var body: some Scene {
         WindowGroup {
             EmojiListView()
         }
+    }
+}
+
+final class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        requestAuthorizationForNotifications()
+        return true
+    }
+    
+    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data)          {
+        print(deviceToken)
+    }
+    
+    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
+        print(error.localizedDescription)
+    }
+    
+    private func requestAuthorizationForNotifications() {
+        UNUserNotificationCenter
+            .current()
+            .requestAuthorization(options: [.alert, .sound, .badge]) { allowed, error in
+                if allowed {
+                    os_log(.debug, "Allowed")
+                    UNUserNotificationCenter.current().delegate = self
+                } else {
+                    os_log(.debug, "Error")
+                }
+            }
+    }
+}
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        completionHandler([.banner, .sound, .badge])
+    }
+    
+    func application(_ application: UIApplication,
+                     didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+        os_log(.debug, "Receive")
     }
 }
