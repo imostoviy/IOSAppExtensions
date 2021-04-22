c18d6af52a78a755ac331de8201ea94c867c5eba
imostoviy <ihor.mostovyi@ekreative.com>
2021-04-22 21:27:15 +0300

    Initial Commit

---
+++ IOSAppExtensions/IOSAppExtensionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAppExtensionsApp.swift
+//  IOSAppExtensions
+//
+//  Created by Ihor Mostovyi on 22.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct IOSAppExtensionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
